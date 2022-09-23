2a37a3520c2ee7f17ba6682a89b341ae237e9c9b
likeep <keepcrimson@naver.com>
2022-09-23 15:15:19 +0900

    SEO git commit 0923 1514i

---
+++ Final/src/main/java/com/Final/Final1/comm/service/MainService.java
@@ -0,0 +1,10 @@
+package com.Final.Final1.comm.service;
+
+import java.util.List;
+
+import com.Final.Final1.comm.model.MainDTO;
+
+public interface MainService {
+
+    List<MainDTO> list();
+}
---
+++ Final/src/main/java/com/Final/Final1/comm/serviceImpl/MainServiceImpl.java
@@ -0,0 +1,27 @@
+package com.Final.Final1.comm.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Final.Final1.comm.model.MainDAO;
+import com.Final.Final1.comm.model.MainDTO;
+import com.Final.Final1.comm.service.MainService;
+
+@Service
+public class MainServiceImpl implements MainService{
+    @Autowired
+    MainDAO MainDAO;
+
+    @Override
+    public List<MainDTO> list() {
+        return MainDAO.list();
+    }
+}
+
+
+
+
+
+
