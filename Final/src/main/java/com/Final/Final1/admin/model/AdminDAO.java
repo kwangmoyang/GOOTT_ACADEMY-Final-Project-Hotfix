a67856a945baa1fe52bbe4694c86156303dd8b20
kwangmoyang <zjqtm45@naver.com>
2022-09-29 17:34:34 +0900

    5:34분 양광모 마지막설정

---
+++ Final/src/main/java/com/Final/Final1/admin/model/AdminDAO.java
@@ -0,0 +1,10 @@
+package com.Final.Final1.admin.model;
+
+import java.util.List;
+import java.util.Map;
+
+public interface AdminDAO {
+
+   List<Map<String, Object>> adminMemList(Map<String, Object> map);
+
+}
---
+++ Final/src/main/java/com/Final/Final1/admin/model/AdminDAOImpl.java
@@ -0,0 +1,22 @@
+package com.Final.Final1.admin.model;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AdminDAOImpl implements AdminDAO {
+
+   @Inject
+   SqlSessionTemplate sqlsession;
+   
+   @Override
+   public List<Map<String, Object>> adminMemList(Map<String, Object> map) {
+      return sqlsession.selectList("admin.list",map);
+   }
+
+}
