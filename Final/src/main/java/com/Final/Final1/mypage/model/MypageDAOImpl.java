a672ab8570006bdcdad24c4f8f5cc6e77550e268
likeep <keepcrimson@naver.com>
2022-09-29 16:45:36 +0900

    SEO git commit 0929 1645i

--- Final/src/main/java/com/Final/Final1/mypage/model/MypageDAOImpl.java
+++ Final/src/main/java/com/Final/Final1/mypage/model/MypageDAOImpl.java
@@ -57,8 +57,8 @@ public class MypageDAOImpl implements MypageDAO {
 	@Override
 
 	public void UserDelete(String userid) {
-		sqlSession.selectOne("userinfo.UserDelete",userid);
-
+		sqlSession.selectOne("userinfo.UserDelete", userid);
+	}
 	public List<BoardDTO> myRequestlist(MyWriterListDTO dto) {
 		return sqlSession.selectList("userinfo.myWriterlist",dto);
 	}
